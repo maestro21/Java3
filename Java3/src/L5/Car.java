f9fd984cac9bd2e5131c41395113e38ce6f49897
Maestro <sergeyspopov@gmail.com>
2020-05-28 12:03:23 +0200

    гонка закончилась

--- Java3/src/L5/Car.java
+++ Java3/src/L5/Car.java
@@ -1,5 +1,7 @@
 package L5;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -8,15 +10,17 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    CountDownLatch cdl;
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed,  CountDownLatch cdl) {
         this.race = race;
         this.speed = speed;
+        this.cdl = cdl;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
@@ -32,5 +36,6 @@ public class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        cdl.countDown();
     }
 }
--- Java3/src/L5/MainClass.java
+++ Java3/src/L5/MainClass.java
@@ -1,19 +1,25 @@
 package L5;
 
 
+import java.util.concurrent.CountDownLatch;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
+
+        final CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), cdl);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        cdl.await();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
